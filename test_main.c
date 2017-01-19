8acc28627d76c198cdabc68a5add663abce59260
Bret MILLER <abunai@sashizu.me>
2017-01-19 00:48:04 -0800

    omg it almost works now

--- test_main.c
+++ test_main.c
@@ -6,7 +6,7 @@
 /*   By: bmiller <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/15 16:48:17 by bmiller           #+#    #+#             */
-/*   Updated: 2017/01/18 16:39:00 by bmiller          ###   ########.fr       */
+/*   Updated: 2017/01/19 00:06:59 by bmiller          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,13 @@ int		main(int argc, char **argv)
 {
 	int		fd;
 	char	**line;
+	char	nl[128];
 
+	line = (char**)(malloc(sizeof(char*)));
+	*line = nl;
 	if (argc < 2)
 		printf("usage: %s: file\n", argv[0]);
 	if ((fd = open(argv[1], O_RDONLY)) == -1)
-		return (-1);
-	if (!(line = (char**)malloc(sizeof(char*))))
-		return (-1);
-	if (!(*line = (char*)malloc(128)))
 		return (-1);
 	while (get_next_line(fd, line) > 0)
 		printf("%s\n", *line);
